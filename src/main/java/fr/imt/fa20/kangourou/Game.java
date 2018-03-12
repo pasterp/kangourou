33d5b80d888486c4664c7d5ea50557d6304c05d5
[a678318] William Jedrzejak <william.jedrzejak@atos.net>
2018-03-12 15:23:32 +0100

    player class

--- src/main/java/fr/imt/fa20/kangourou/Game.java
+++ src/main/java/fr/imt/fa20/kangourou/Game.java
@@ -17,78 +17,78 @@ public class Game extends BasicGame {
 	private static final int HEIGHT = 600;
 
 	private int gravity = 3;
+	
+	/** Player **/
+	private Player player;
 
 	/** Game... */
 	private GameContainer container;
 	private TiledMap map;
 
-	/** Personnage **/
-	private float x = 300, y = 300;
-	private int direction = 0;
-	private boolean moving = false;
-	private Animation animation = new Animation();
+	
 
 	/** Keys **/
 	private boolean isLeftPressed = false, isRightPressed = false;
 
 	public Game() {
 		super("Slick Kangourou");
-	}
-
-	public void render(GameContainer container, Graphics g) throws SlickException {
-		g.translate(container.getWidth() / 2 - this.x, container.getHeight() / 2 - this.y);
-		this.map.render(0, 0);
-
-		g.drawAnimation(animation, x - 50, y - 130);
-		g.setColor(new Color(255, 0, 0, 1.0f));
-		g.fillOval(x, y, 2, 2);
+		
 	}
 
 	@Override
-	public void init(GameContainer container) throws SlickException {
+	public void init(GameContainer container) throws SlickException  {
 		this.container = container;
 		this.map = new TiledMap("map/map1.tmx");
-		SpriteSheet spriteSheet = new SpriteSheet("sprite/kangaroo.png", 100, 130);
-		Animation animation = new Animation();
-		animation.addFrame(spriteSheet.getSprite(0, 0), 500);
-		animation.addFrame(spriteSheet.getSprite(1, 0), 200);
-		this.animation = animation;
+		 player = new Player();
+		
 	}
+	
+	
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		g.translate(container.getWidth() / 2 - player.x, container.getHeight() / 2 - player.y);
+		this.map.render(0, 0);
+		
+		g.drawAnimation(player.getAnimation(), player.x - 50, player.y - 130);
+		g.setColor(new Color(255, 0, 0, 1.0f));
+		g.fillOval(player.x, player.y, 2, 2);
+	}
+
+	
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
 		System.out.println("left:" + this.isLeftPressed + "  right:" + this.isRightPressed);
 
 		if (this.isLeftPressed && !this.isRightPressed) {
-			this.moving = true;
-			this.direction = -1;
+			player.moving = true;
+			player.direction = -1;
 		} else if (this.isRightPressed && !this.isLeftPressed) {
-			this.moving = true;
-			this.direction = 1;
+			player.moving = true;
+			player.direction = 1;
 		} else if (!this.isLeftPressed && !this.isRightPressed) {
-			this.moving = false;
+			player.moving = false;
 		}
 
-		if (this.moving) {
-			float futurX = this.x;
-			futurX = this.x + .2f * delta * this.direction;
+		if (player.moving) {
+			float futurX = player.x;
+			futurX = player.x + .2f * delta * player.direction;
 			Image tile = this.map.getTileImage((int) futurX / this.map.getTileWidth(),
-					(int) this.y / this.map.getTileHeight(), this.map.getLayerIndex("logic"));
+					(int) player.y / this.map.getTileHeight(), this.map.getLayerIndex("logic"));
 			if (tile != null) {
-				this.moving = false;
+				player.moving = false;
 			} else {
-				this.x = futurX;
+				player.x = futurX;
 			}
 		}
 
-		float futurY = this.y;
+		float futurY = player.y;
 		futurY += .2f * delta * gravity;
-		Image tile = this.map.getTileImage((int) this.x / this.map.getTileWidth(),
+		Image tile = this.map.getTileImage((int) player.x / this.map.getTileWidth(),
 				(int) futurY / this.map.getTileHeight(), this.map.getLayerIndex("logic"));
 		if (tile != null) {
-			this.moving = false;
+			player.moving = false;
 		} else {
-			this.y = futurY;
+			player.y = futurY;
 		}
 		
 
@@ -123,8 +123,8 @@ public class Game extends BasicGame {
 		}
 
 		if (this.isLeftPressed && this.isRightPressed) {
-			this.moving = true;
-			this.direction *= -1;
+			player.moving = true;
+			player.direction *= -1;
 		}
 	}
 
