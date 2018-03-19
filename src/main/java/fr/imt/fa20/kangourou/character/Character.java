9e28a7a444709895c5d4131226c3d9bd43322f81
[a678318] William Jedrzejak <william.jedrzejak@atos.net>
2018-03-19 15:26:28 +0100

    collisions fonctionneles avec hitbox

--- src/main/java/fr/imt/fa20/kangourou/character/Character.java
+++ src/main/java/fr/imt/fa20/kangourou/character/Character.java
@@ -3,17 +3,22 @@ package fr.imt.fa20.kangourou.character;
 import org.newdawn.slick.Animation;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
 
-import fr.imt.fa20.kangourou.character.AnimationsLoader.AnimationsLoader;
 import fr.imt.fa20.kangourou.map.Map;
 
 public abstract class Character {
-	private float x = 50, y = 50;
+	private float x = 50, y = 60;
 	private Direction direction = Direction.RIGHT;
 	private Map map;
+	
+	protected Rectangle hitbox;
 
 	public Character(Map map) {
 		this.map = map;
+		
+		this.hitbox = new Rectangle(this.getX()-8, this.getY()-20, 15, 20);
+		
 	}
 
 	public abstract void init() throws SlickException;
@@ -29,6 +34,8 @@ public abstract class Character {
 	public void render(Graphics g) {
 		// coordonnées d'affichage correspondant aux pieds du personnage
 		g.drawAnimation(this.getAnimation(), (int) x - 16, (int) y - 32);
+		
+		g.draw(hitbox);
 	}
 
 	public float getX() {
