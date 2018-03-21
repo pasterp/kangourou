1f04bff8b725d2da8f931636a7b4828779cfa685
[a678318] William Jedrzejak <william.jedrzejak@atos.net>
2018-03-21 15:23:02 +0100

    colisions en cours

--- src/main/java/fr/imt/fa20/kangourou/map/Map.java
+++ src/main/java/fr/imt/fa20/kangourou/map/Map.java
@@ -8,7 +8,10 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.GeomUtil;
 import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Transform;
 import org.newdawn.slick.tiled.TiledMap;
 import org.w3c.dom.css.Rect;
 
@@ -25,14 +28,13 @@ public class Map {
 
 		for (int x = 0; x < tiledMap.getWidth(); x++) {
 			for (int y = 0; y < tiledMap.getHeight(); y++) {
-				Image tile = tiledMap.getTileImage( x , y , logicLayer);
+				Image tile = tiledMap.getTileImage(x, y, logicLayer);
 				if (tile != null) {
 					collisions.add(new Rectangle(x * tileSize, y * tileSize, tileSize, tileSize));
-				} else {
-					System.out.println("else");
 				}
 			}
 		}
+
 	}
 
 	public void renderBackground() {
@@ -57,7 +59,7 @@ public class Map {
 		// grass
 		this.tiledMap.render(0, 0, 7);
 
-		for (Rectangle r : collisions) {
+		for (Shape r : collisions) {
 			g.setColor(Color.red);
 			g.draw(r);
 		}
